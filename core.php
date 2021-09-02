eafbae142ca51b3e619cdf63ba4073913b00b153
feelgoodincorp <newuser96@ukr.net>
2021-09-02 21:08:07 +0300

    test2

---
+++ core.php
@@ -0,0 +1,28 @@
+<?php 
+
+
+echo date('H:i:s');
+
+// //указываем что страница возвращает json(нужно попробовать с xml)
+header("Content-type: application/json; charset=utf-8");
+
+class Page{
+
+	public $title;
+	public $body;
+
+	public function __construct($title, $body){
+		$this->title = $title;
+		$this->body = $body;
+	}
+}
+
+$item_data = [new Page('test1','<p>test1</p>'), new Page('test2','<p>test2</p>')];
+
+$id = $_GET["0"];
+
+$response = $item_data[$id - 0];
+
+die(json_encode($response));
+
+?>
---
+++ script.js
@@ -0,0 +1,22 @@
+var data = {
+	title: "",
+	item_data: "",
+	link: ""
+};
+
+var page = {
+	// title: document.querySelector("title");
+	// item_data: document.getElementByClassName("item");
+}
+
+function loadData(){
+	console.log("data");
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function(){
+		if(this.readyState == 4 && this.status == 200){
+			document.getElementById('item').innerHTML = this.responseText;
+		}
+	};
+	xhttp.open("GET","info.txt", true);
+	xhttp.send();
+}
